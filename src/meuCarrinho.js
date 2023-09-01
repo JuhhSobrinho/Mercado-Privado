d7c75787381cbc1437471cd0ecfe336731113d3f
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2023-09-01 00:22:07 -0300

    tem erro meu chama, no desenharprodutocarrinho, no bd.find dele está dando como indefinido quando aconte um refresc nele, tipo ele limpa quando é um elemento, quando quando é mais de uma ele da erro, como se não tivesse mais achando o banco, tentei definir o banco pelo refresc(renderizarCarrinhos), mas não funcionou muito bem. enfim bucha para amanhã.

--- src/meuCarrinho.js
+++ src/meuCarrinho.js
@@ -1,3 +1,12 @@
+import { carregarBancoDeDados } from '../model/model';
+
+carregarBancoDeDados()
+  .then(bd => {
+    renderizarCarrinho(bd);
+  });
+
+const idsProdutoCarrinhoComQuantidade = {};
+
 export function meuCarrinho(bd) {
     const abrirCarrinho = document.querySelector('#abrirCarrinho');
     const closeCarrinho = document.querySelector('#closeCarrinho');
@@ -22,7 +31,32 @@ export function meuCarrinho(bd) {
 
 }
 
-export function adicionarAoCarrinho(idProduto, bd) {
+/////////////////////// Aumenta numero no ***carrinho***, ***diminui*** e ***atualiza*** o nuemro que mostra no card
+function removerDoCarrinho(idProduto) {
+    delete idsProdutoCarrinhoComQuantidade[idProduto];
+    renderizarCarrinho();
+}
+
+function incrementarQuantidadeProduto(idProduto) {
+    idsProdutoCarrinhoComQuantidade[idProduto]++;
+    atualizarInfoQuantidade(idProduto);
+};
+
+function decrementarQuantidadeProduto(idProduto) {
+    idsProdutoCarrinhoComQuantidade[idProduto]--;
+    atualizarInfoQuantidade(idProduto);
+};
+
+function atualizarInfoQuantidade(idProduto) {
+    const quantidadeDeTalItem = document.querySelector(`#numbPecas${idProduto}`);
+    quantidadeDeTalItem.innerHTML = idsProdutoCarrinhoComQuantidade[idProduto];
+}
+/////////////////////// 
+
+
+
+
+function desenharProdutoCarrinho(idProduto, bd) {
     function tipo(tipo) {
         if (tipo) {
             return 'feminino';
@@ -33,25 +67,89 @@ export function adicionarAoCarrinho(idProduto, bd) {
 
     // find(percorre o banco e verifica se o idProduto, cujo é enviado pelo main, é igual a algum id dentro de BD)
     const produto = bd.find((produto) => produto.id === idProduto);
-
-    console.log(produto.nome);
     const containerProdutosCarrinho = document.querySelector('#produtosCarrinho');
-    const cartaoProdutoCarrinho = `<article class="carrProduto">
+
+    ///////////////////     CRIANDO HTML no js ao invez de tacar texto no html e o baguio precisar desmontar e montar
+    const elementoArticleHTML = document.createElement("article");  //<artcle></article>
+    elementoArticleHTML.classList.add("carrProduto");
+    ////////////////// o conteudo dentro do cartão não precisa mudar para html, texto já funfa
+    const cartaoProdutoCarrinho = `
+        <div class="imgComDescricao">
+            <img src="./assets/img/${produto.imagem}.jpg" class="carrImg" alt="Image">
+            <button id="removeItem${produto.id}">
+                <img src="./assets/logo/removeItem.svg" alt="Image">
+            </button>
+            <div class="carrDescricao">
+                <p class="carrDados" id="carrNome">${produto.nome}</p>
+                <p class="carrDados" id="carrPreco">R$: ${produto.preco}</p>
+                <p class="carrDados" id="carrTipo">Tipo: ${tipo(produto.tipo)}</p>
+                <p class="carrDados" id="carrMarca">Marca: ${produto.marca}</p>
+            </div>
+        </div>
+        <div class="numbProdutos">
+            <button class="numbDados" id="botaoMenos${produto.id}"> - </button>
+            <p class="numbDados" id="numbPecas${produto.id}">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+            <button class="numbDados" id="adicionarMaisItem${produto.id}"> + </button>
+        
+        </div>`;
+
+    elementoArticleHTML.innerHTML = cartaoProdutoCarrinho;
+    containerProdutosCarrinho.appendChild(elementoArticleHTML);
+
+    const botaoRemoveItem = document.querySelector(`#removeItem${produto.id}`);
+    const botaoAdicionar = document.querySelector(`#adicionarMaisItem${produto.id}`);
+    const botaoMenos = document.querySelector(`#botaoMenos${produto.id}`);
+
+    botaoRemoveItem.addEventListener("click", () => {
+        removerDoCarrinho(idProduto);
+    });
+
+    botaoAdicionar.addEventListener("click", () => {
+        incrementarQuantidadeProduto(`${produto.id}`);
+    });
+
+    botaoMenos.addEventListener("click", () => {
+        decrementarQuantidadeProduto(`${produto.id}`);
+
+        if (idsProdutoCarrinhoComQuantidade[idProduto] === 0) {
+            removerDoCarrinho(idProduto);
+            return;
+        }
+        console.log(idsProdutoCarrinhoComQuantidade[idProduto]);
+    });
+
+    return;
+
+}
 
 
-    <img src="./assets/img/${produto.imagem}.jpg" class="carrImg" alt="Image">
-    <button id="removeItem">
-        <img src="./assets/logo/removeItem.svg" alt="Image">
-    </button>
-    <div class="carrDescricao">
-      <p class="carrDados" id="carrNome">${produto.nome}</p>
-      <p class="carrDados" id="carrPreco">R$: ${produto.preco}</p>
-      <p class="carrDados" id="carrTipo">Tipo: ${tipo(produto.tipo)}</p>
-      <p class="carrDados" id="carrMarca">Marca: ${produto.marca}</p>
-    </div>
-  </article>`;
+function renderizarCarrinho(bd) {
+    const containerProdutosCarrinho = document.querySelector('#produtosCarrinho');
+    containerProdutosCarrinho.innerHTML = ``;
 
-    console.log(bd);
+    console.log(idsProdutoCarrinhoComQuantidade);
 
-    containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+
+    for (const ids in idsProdutoCarrinhoComQuantidade) {
+        console.log(idsProdutoCarrinhoComQuantidade);
+        desenharProdutoCarrinho(ids, bd);
+    }
+
+
+}
+
+
+export function adicionarAoCarrinho(idProduto, bd) {
+    if (idProduto in idsProdutoCarrinhoComQuantidade) {
+        incrementarQuantidadeProduto(idProduto);
+
+        return; // finaliza o processo e stop a função, assim não executando o codigo fora do if
+    }
+
+
+
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+
+    desenharProdutoCarrinho(idProduto, bd); // Obtém o botão do objeto retornado
+
 }
